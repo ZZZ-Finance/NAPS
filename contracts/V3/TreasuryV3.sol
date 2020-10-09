7e0b3a3dcd6358c80071b71283dffc5bbe3b26a0
AndreZZZ123 <adrezzz123@gmail.com>
2020-10-09 17:32:07 +0800

    Update TreasuryV3.sol

--- contracts/V3/TreasuryV3.sol
+++ contracts/V3/TreasuryV3.sol
@@ -13,7 +13,7 @@ interface IERC20 {
 contract Context {
     constructor () internal { }
     // solhint-disable-previous-line no-empty-blocks
-
+}
 /**
  * @dev Contract module which provides a basic access control mechanism, where
  * there is an account (an owner) that can be granted exclusive access to
